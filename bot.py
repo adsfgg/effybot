769fefb24b8d25733324d6449f2510180304b5fd
adsfgg <11364579+adsfgg@users.noreply.github.com>
2018-10-27 20:03:15 +0100

    Added option to select which channels to listen to with a command

--- bot.py
+++ bot.py
@@ -44,9 +44,6 @@ with open("token.txt") as f:
 ASDF_ID = "134781032086896641"
 BIG_BRAIN_ID = 421464243339001860
 BOT_ERRORS_ID = 482931487767920640
-BOT_TESTING_ID = 482913139759906836
-BOT_CENTRAL_ID = 446243019205640212
-ALLOWED_CHANNELS_ID = [BOT_TESTING_ID, BOT_CENTRAL_ID]
 PREFIX = "!"
 
 initial_extensions = []
@@ -58,6 +55,8 @@ with open("initial_extensions.txt") as f:
 		initial_extensions.append(line[:-1]) #strip newline
 
 logger.info("Loaded.")
+
+from bot_commands import *
 
 bot = commands.Bot(command_prefix=PREFIX)
 
@@ -91,26 +90,62 @@ async def on_voice_state_update(member, before, after):
 		await remove_from_text(member)
 
 async def remove_from_text(user):
-	logger.info(f"{user} left big brains, removing perms")
-	
 	channel = discord.utils.get(user.guild.channels, name="big-text")
 
 	await channel.set_permissions(user, read_messages=False)
 
 async def add_to_text(user):
-	logger.info(f"{user} joined big brains, adding perms")
-
 	channel = discord.utils.get(user.guild.channels, name="big-text")	
 
 	await channel.set_permissions(user, read_messages=True)
 
 @bot.event
 async def on_message(message):
+	cmd, args = get_command(message)
+
 	if message.author.bot: return
 	if bot.user.mentioned_in(message) and message.content.startswith(f'<@{bot.user.id}>') and message.mention_everyone is False:
 		await message.channel.send(f'{message.author.mention}, my prefix is "{PREFIX}"')
+	elif cmd is not None:
+		await process_bot_command(message, cmd, args)
 	elif message.channel.id in ALLOWED_CHANNELS_ID:
 		await bot.process_commands(message)
+
+def get_command(message):
+	for cmd in bot_commands:
+		cmd_name = f"{PREFIX}{cmd['name']}"
+		if message.content.startswith(f"{cmd_name} ") or message.content == f"{cmd_name}":
+			args = message.content.split(" ")[1:]
+
+			return cmd, args
+
+	return None, None
+
+async def process_bot_command(message, command, args):
+	if command is not None:
+		ctx = await bot.get_context(message)
+		ctx.command = command
+		ctx.args = args
+
+		if command["has_args"]:
+			argc = len(ctx.args)
+			min_args = command["min_args"]
+			max_args = command["max_args"]
+
+			if argc < min_args:
+				return await ctx.send("Not enough arguments. Needs at least {0} argument{1}".format(min_args, "s" if min_args > 1 else ""))
+			elif argc > max_args:
+				return await ctx.send("Too many arguments. Needs at most {0} argument{1}".format(max_args, "s" if max_args > 1 else ""))
+
+			valid_args = command["valid_args"]
+
+			for arg in args:
+				if arg not in valid_args:
+					return await ctx.send("Invalid argument. Must be one of {0}".format(valid_args))
+
+		on_command_func = ctx.command["on_command"]
+
+		await on_command_func(ctx)
 
 @bot.event
 async def on_ready():
---
+++ bot_commands.py
@@ -0,0 +1,40 @@
+BOT_TESTING_ID = 482913139759906836
+BOT_CENTRAL_ID = 446243019205640212
+ALLOWED_CHANNELS_ID = [BOT_TESTING_ID, BOT_CENTRAL_ID]
+
+#TODO: Make this use embeds
+#TODO: Make bot remember channels between restarts
+
+async def on_cmd_channel(ctx):
+	arg = ctx.args[0] #there should only be one argument for this cmd
+	cid = ctx.channel.id
+
+	if arg == "add":
+		if not cid in ALLOWED_CHANNELS_ID:
+			ALLOWED_CHANNELS_ID.append(cid)
+			return await ctx.send("Now listening in this channel for commands. :blush:")
+		else:
+			return await ctx.send("I'm already listening in this channel :wink:")
+	elif arg == "remove":
+		if cid in ALLOWED_CHANNELS_ID:
+			ALLOWED_CHANNELS_ID.remove(cid)
+			return await ctx.send("Stopped listening in this channel for commands. :sob:")
+		else:
+			return await ctx.send("I'm not listening in this channel :blush:")
+	elif arg == "list":
+		return await ctx.send("not in yet ;)")
+
+bot_commands = [
+	{
+		"name": "channel",
+		"has_args": True,
+		"max_args": 1,
+		"min_args": 1,
+		"valid_args": [
+			"add",
+			"remove",
+			"list"
+		],
+		"on_command": on_cmd_channel
+	}
+]
