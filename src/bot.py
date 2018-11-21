7517fff9c49fc391e6d60063bb9acac242ae93fc
adsfgg <11364579+adsfgg@users.noreply.github.com>
2018-11-21 21:26:49 +0000

    move prefix to settings.json

--- src/bot.py
+++ src/bot.py
@@ -13,11 +13,106 @@ logging_level = logging.DEBUG
 OWNER_ID = ""
 BIG_BRAIN_ID = 421464243339001860
 BOT_ERRORS_ID = 482931487767920640
-PREFIX = "!"
+PREFIX = ""
 pingbm = "What the fuck did you just @ me for, you little pinger? I’ll have you know I graduated top of my class in the @ spamming, and I’ve been involved in numerous secret raids on shitty minecraft servers, and I have over 300 confirmed @ pings. I am trained in @ warfare and I’m the top @ spammer in the entire @ spamming forces. You are nothing to me but just another person annoyed by the @ pings. I will @ you the fuck out with @ spam in the likes of which has never @'d before on this Earth, mark my fucking @ ping. You think you can get away with kick me over discord? Think again, fucker. As we speak I am activating secret network of bots and your server invite link is being traced right now so so you better prepare for about 100+ @ pings. The @ pings that wipes out the pathetic little thing you call your server. You’re fucking @'d, kiddo. I can @ you anywhere, anytime, and I can @ ping your server in over seven hundred ways, and that’s just with my bare keyboard. Not only am I extensively trained in CNTRL-C and CNTRL-V, but I have access to the entire arsenal of the entire Discord @ code and I will use it to its full extent to @ your miserable ass off the face of Discord, you little shit. If only you could have known what unholy @ pinging your little “clever” kick from the server was about to ping down upon you, maybe you would have held your fucking keyboard."
 initial_extensions = []
 
+# bot setup
+
+def setup_logging():
+  logger = logging.getLogger("Discord_Bot")
+  logger.setLevel(logging_level)
+  
+  #log file
+  fh = logging.FileHandler("log.txt")
+  fh.setLevel(logging_level)
+  
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  
+  #console log
+  if use_console_logging:
+    ch = logging.StreamHandler()
+    ch.setLevel(logging_level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+  
+  fh.setFormatter(formatter)
+
+  logger.addHandler(fh)
+
+  logger.info("Logging setup successfully")
+
+  return logger
+
+def load_bot_creds():
+  settings = []
+  with open("configs/bot.json") as f:
+    settings = json.load(f)
+  return settings
+
+def load_bot_settings():
+  settings = []
+  with open("configs/settings.json") as f:
+    settings = json.load(f)
+  return settings
+
+def main():
+  global logger, use_console_loggin, PREFIX, OWNER_ID
+  TOKEN = ""
+  
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-c", "--no-console", required=False, help="Disables console output")
+  args = vars(ap.parse_args())
+  
+  if args["no_console"]:
+    use_console_logging = False
+
+  logger = setup_logging()
+
+  logger.info("Using Discord.py ({0})".format(discord.__version__))
+
+  creds = load_bot_creds()
+  settings = load_bot_settings()
+
+  bot_name = creds["name"]
+  TOKEN = creds["token"]
+  OWNER_ID = creds["token"]
+
+  logger.info("Loaded credentials")
+
+  PREFIX = settings["prefix"]
+
+  logger.info("Loaded settings")
+
+  logger.info("Loading initial_extensions...")
+
+  with open("src/initial_extensions.txt") as f:
+    for line in f:
+      initial_extensions.append(line[:-1]) #strip newline
+
+    logger.info("Loaded.")
+
+    logger.info("loading extensions")
+
+  for extension in initial_extensions:
+    try:
+      bot.load_extension(extension)
+      logger.info(f'Loaded extension {extension}.')
+      
+    except ModuleNotFoundError as e:
+      logger.warning(f'Failed to load extension {extension}.')
+      logger.debug(e)
+
+  logger.info("Starting bot")
+
+  bot.run(TOKEN)
+
+if __name__ == "__main__":
+  main()
+
 bot = commands.Bot(command_prefix=PREFIX)
+
+# bot event handling
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -110,85 +205,3 @@ async def process_bot_command(message, command, args):
 async def on_ready():
   logger.info("Logged in as {0.name} ({0.id})".format(bot.user))
   await bot.change_presence(activity=discord.Game(name="0w0 what's this"))
-
-def setup_logging():
-  logger = logging.getLogger("Discord_Bot")
-  logger.setLevel(logging_level)
-  
-  #log file
-  fh = logging.FileHandler("log.txt")
-  fh.setLevel(logging_level)
-  
-  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-  
-  #console log
-  if use_console_logging:
-    ch = logging.StreamHandler()
-    ch.setLevel(logging_level)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-  
-  fh.setFormatter(formatter)
-
-  logger.addHandler(fh)
-
-  logger.info("Logging setup successfully")
-
-  return logger
-
-def load_bot_settings():
-  settings = []
-  with open("configs/bot.json") as f:
-    settings = json.load(f)
-  return settings
-
-def main():
-  global logger, use_console_loggin, PREFIX, OWNER_ID
-  TOKEN = ""
-  
-  ap = argparse.ArgumentParser()
-  ap.add_argument("-c", "--no-console", required=False, help="Disables console output")
-  args = vars(ap.parse_args())
-
-  if args["no_console"]:
-    use_console_logging = False
-  
-  logger = setup_logging()
-
-  logger.info("Using Discord.py ({0})".format(discord.__version__))
-
-  settings = load_bot_settings()
-
-  bot_name = settings["name"]
-  TOKEN = settings["token"]
-  OWNER_ID = settings["token"]
-  PREFIX = settings["prefix"]
-
-
-  logger.info("Loaded credentials")
-
-  logger.info("Loading initial_extensions...")
-
-  with open("src/initial_extensions.txt") as f:
-    for line in f:
-      initial_extensions.append(line[:-1]) #strip newline
-
-  logger.info("Loaded.")
-  
-  logger.info("loading extensions")
-
-  for extension in initial_extensions:
-    try:
-      bot.load_extension(extension)
-      logger.info(f'Loaded extension {extension}.')
-
-    except ModuleNotFoundError as e:
-      logger.warning(f'Failed to load extension {extension}.')
-      logger.debug(e)
-
-  logger.info("Starting bot")
-
-  bot.run(TOKEN)
-
-if __name__ == "__main__":
-  main()
